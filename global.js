5f93fe9427b7670d58525b26f8431e1beb17df72
Jocelyn Ng <jon008@ucsd.edu>
2026-04-15 19:53:40 -0700

    Use JS to automate current page link

--- global.js
+++ global.js
@@ -6,3 +6,14 @@ console.log('IT’S ALIVE!');
 function $$(selector, context = document) {
   return Array.from(context.querySelectorAll(selector));
 }
+
+// Step 2.1: Get an array of all navigation links
+let navLinks = $$("nav a");
+
+// Step 2.2: Find the link that matches the current page's URL
+let currentLink = navLinks.find(
+    (a) => a.host === location.host && a.pathname === location.pathname
+);
+
+// Step 2.3: Add the 'current' class to that specific link (if found)
+currentLink?.classList.add('current');
